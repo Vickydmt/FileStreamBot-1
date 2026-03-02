4e71b15767012dc652a22bb8c66c184393215a49
S VIVEGANANDAN <111070769+Vickydmt@users.noreply.github.com>
2026-03-03 00:00:38 +0530

    Fix syntax errors in render_template.py

--- WebStreamer/utils/render_template.py
+++ WebStreamer/utils/render_template.py
@@ -14,19 +14,19 @@ async def render_page(db_id):
     src = urllib.parse.urljoin(Var.URL, f'dl/{file_data["_id"]}')
     if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
         async with aiofiles.open('WebStreamer/template/req.html') as r:
-            heading = f'Watch {file_data['file_name']}'
+            heading = f"Watch {file_data['file_name']}"
             tag = (file_data['mime_type']).split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_data['file_name'], src)
     elif str((file_data['mime_type']).split('/')[0].strip()) == 'audio':
         async with aiofiles.open('WebStreamer/template/req.html') as r:
-            heading = f'Listen {file_data['file_name']}'
+            heading = f"Listen {file_data['file_name']}"
             tag = (file_data['mime_type']).split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_data['file_name'], src)
     else:
         async with aiofiles.open('WebStreamer/template/dl.html') as r:
             async with aiohttp.ClientSession() as s:
                 async with s.get(src) as u:
-                    heading = f'Download {file_data['file_name']}'
+                    heading = f"Download {file_data['file_name']}"
                     file_size = humanbytes(int(u.headers.get('Content-Length')))
                     html = (await r.read()) % (heading, file_data['file_name'], src, file_size)
     return html
